2054482b8e18b8777d3907779601e31eec6fd6d0
Kyrylo Hrecheniuk <kyryloh@wix.com>
2019-03-24 13:03:24 +0200

    moved some files and instaled tslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <p>Edit <code>src/App.tsx</code> and save to reload.</p>
           <a
             className="App-link"
             href="https://reactjs.org"

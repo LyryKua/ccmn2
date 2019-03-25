0bf033f48f5cad3162b4e9a53ba4d06dbfa8dea1
Kyrylo Hrecheniuk <kyryloh@wix.com>
2019-03-25 17:11:17 +0200

    deleted useless jest configuration in package.json

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import App from './App';
